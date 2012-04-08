2ee4cf348526ec3e29ef1a49d094c24fbfb31e15
Espen A. Kristiansen <post@espenak.net>
2012-04-08 09:44:25 +0200

    Skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name = 'django_ckeditorfiles',
+      description = 'CKEditor bundled as a django staticfiles app.',
+      version = '1.0',
+      url = 'http://espenak.net',
+      author = 'Espen Angell Kristiansen',
+      license = 'LGPL',
+      packages=find_packages(exclude=['ez_setup']),
+      install_requires = ['setuptools', 'Django']
+)
