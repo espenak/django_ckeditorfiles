eb4f006acf24bad45c29349a66ec5fa6049d7a58
Espen A. Kristiansen <post@espenak.net>
2012-04-08 13:39:23 +0200

    Added a widget.

---
+++ ckeditorfiles/widgets.py
@@ -0,0 +1,37 @@
+import json
+from django.conf import settings
+from django.forms.util import flatatt
+from django.forms.widgets import Textarea
+from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape
+from django.utils.encoding import force_unicode
+from django import forms
+
+
+class CKEditorWidget(Textarea):
+    config = {}
+    def __init__(self, attrs=None, config=None):
+        if config:
+            self.config = config
+        super(CKEditorWidget, self).__init__(attrs)
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        cssclass = 'ckeditorwidget'
+        if 'class' in attrs:
+            cssclass += ' ' + attrs['class']
+        attrs['class'] = cssclass
+        final_attrs = self.build_attrs(attrs, name=name)
+        script = ('\n<script type="text/javascript">\n'
+                  'CKEDITOR.replace("{id}", {config});\n'
+                  '</script>\n').format(id=attrs['id'],
+                                        config=json.dumps(self.config, indent=2))
+        return mark_safe(u'<textarea{attrs}>{value}</textarea>{script}'.format(attrs=flatatt(final_attrs),
+                                                                       value=conditional_escape(force_unicode(value)),
+                                                                       script=script))
+
+    def _media(self):
+        js = [settings.STATIC_URL + '/ckeditorfiles/ckeditor.js']
+        return forms.Media(js=js)
+    media = property(_media)
